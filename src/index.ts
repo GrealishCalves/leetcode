be83c5ce09f1fc9d2bf20b52325b5a2340955a55
GrealishCalves <maximmilshtien@gmail.com>
2023-05-20 18:14:46 +0300

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import fetch from "node-fetch";
+import fetch, { Response } from "node-fetch";
 
-export interface Data {
+export interface Todo {
   userId: number;
   id: number;
   title: string;
@@ -11,29 +11,31 @@ const apiUrl = "https://jsonplaceholder.typicode.com/todos/1";
 
 export async function executeAsync(): Promise<void> {
   try {
-    const data = await fetchDataAsync(apiUrl);
-    logSuccess(data);
-  } catch (error) {
+    const todo: Todo = await fetchTodoAsync(apiUrl);
+    logSuccess(todo);
+  } catch (error: unknown) {
     if (error instanceof Error) {
       handleError(error);
     }
   }
 }
 
-async function fetchDataAsync(url: string): Promise<Data> {
-  const response = await fetch(url);
+async function fetchTodoAsync(url: string): Promise<Todo> {
+  const response: Response = await fetch(url);
+
   if (response.ok) {
-    return (await response.json()) as Data;
+    return (await response.json()) as Todo;
   } else {
     throw new Error(url);
   }
 }
 
 function handleError(error: Error): void {
-  console.error("Error fetching data:", error.message);
+  console.error("Error fetching todo:", error.message);
 }
-function logSuccess(todoData: Data): void {
-  console.log("Data fetched successfully:", todoData);
+
+function logSuccess(todo: Todo): void {
+  console.log("Todo fetched successfully:", todo);
 }
 
 void executeAsync();
